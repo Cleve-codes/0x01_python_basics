1b3ea7a6f0dac178e1ffe4faa403a978a01e7fe5
Cleve-codes <clevemomanyi@gmail.com>
2024-05-25 14:06:51 +0300

    password generator2

--- string_n_number_manipulation/password_generator.py
+++ string_n_number_manipulation/password_generator.py
@@ -2,8 +2,20 @@ import secrets
 import string
 
 
-def generate_password(length: int) -> str:
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+# def generate_password(length: int) -> str:
+#     alphabet = string.ascii_letters + string.digits + string.punctuation
+#     return ''.join(secrets.choice(alphabet) for _ in range(length))
+
+def generate_password(length: int, uppercase: bool = True, lowercase: bool = True, digits: bool = True, special: bool = True) -> str:
+    alphabet = ''
+    if uppercase:
+        alphabet += string.ascii_uppercase
+    if lowercase:
+        alphabet += string.ascii_lowercase
+    if digits:
+        alphabet += string.digits
+    if special:
+        alphabet += string.punctuation
     return ''.join(secrets.choice(alphabet) for _ in range(length))
 
 
