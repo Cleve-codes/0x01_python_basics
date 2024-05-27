c1da39047314d1c3533563bc7bc914a0e940c11a
Cleve-codes <clevemomanyi@gmail.com>
2024-05-27 07:26:37 +0300

    spa

--- algorithm_design/shortest_path_first.py
+++ algorithm_design/shortest_path_first.py
@@ -1,11 +1,13 @@
 my_graph = {
-    'A': [('B', 1), ('D', 4)],
-    'B': [('A', 1), ('C', 2)],
-    'C': [('B', 2), ('D', 5)],
-    'D': [('A', 4), ('C', 5)]
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C', 1), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
 }
 
-def shortest_path(graph, start):
+def shortest_path(graph, start, target = ''):
   # unvisited = []
   unvisited = list(graph)
   distances = {node: 0 if node == start else float('inf') for node in graph}
@@ -25,13 +27,20 @@ def shortest_path(graph, start):
     for node, distance in graph[current]:
       if distances[current] + distance < distances[node]:
         distance[node] = distance[current] + distance
-        if paths[node][-1] == node:
-          paths[node] = paths[current]
+        if paths[node] and paths[node][-1] == node:
+          paths[node] = paths[current][:]
         else:
           paths[node].extend(paths[current])
       paths[node].append(node)
     unvisited.remove(current)
 
-  print(f'Unvisited: {unvisited}\nDistance: {distances}\nPaths: {paths}')
+  # print(f'Unvisited: {unvisited}\nDistance: {distances}\nPaths: {paths}')
+  targets_to_print = [target] if target else graph
+  for node in targets_to_print:
+    if node == start:
+      continue
+      print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
+
+  return distances, paths
 
 shortest_path(my_graph, 'A')
