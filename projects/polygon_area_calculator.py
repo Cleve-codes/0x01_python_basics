c3c42c4c450a453759a35e31aa4c2b83c8b8a1e9
Cleve-codes <clevemomanyi@gmail.com>
2024-05-31 23:01:33 +0300

    polygon-area-calculator-v1

--- projects/polygon_area_calculator.py
+++ projects/polygon_area_calculator.py
@@ -115,3 +115,22 @@ class Square(Rectangle):
   def set_height(self, side):
     self.width = side
     self.height = side
+
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+
+rect.set_height(3)
+print(rect.get_perimeter())
+
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+
+sq.set_side(4)
+print(sq.get_diagonal())
+
+print(sq)
+print(sq.get_picture())
