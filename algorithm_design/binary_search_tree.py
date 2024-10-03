e4e7bed2175efdd6a06c77beccceb9dbaf26ef08
Cleve Momanyi <105583380+Cleve-codes@users.noreply.github.com>
2024-10-03 21:04:54 +0300

    Update binary_search_tree.py

--- algorithm_design/binary_search_tree.py
+++ algorithm_design/binary_search_tree.py
@@ -8,6 +8,15 @@ class TreeNode:
   def __str__(self):
     return str(self.key)
 
+class TreeNode:
+  def __init__(self, key):
+    self.key = key
+    self.left = None
+    self.right = None
+
+  def __str__(self):
+    return str(self.key)
+
 class BinarySearchTree:
   def __init__(self):
     self.root =None
